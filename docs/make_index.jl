51fee13bc0f1518b2f846298e90d15584fdd147a
mtfishman <mfishman@flatironinstitute.org>
2025-03-23 16:31:28 -0400

    Update license, acknowledgements, docs logos

--- docs/make_index.jl
+++ docs/make_index.jl
@@ -1,9 +1,20 @@
 using Literate: Literate
 using BackendSelection: BackendSelection
 
+function ccq_logo(content)
+  include_ccq_logo = """
+    ```@raw html
+    <img src="assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">
+    ```
+    """
+  content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)
+  return content
+end
+
 Literate.markdown(
   joinpath(pkgdir(BackendSelection), "examples", "README.jl"),
   joinpath(pkgdir(BackendSelection), "docs", "src");
   flavor=Literate.DocumenterFlavor(),
   name="index",
+  postprocess=ccq_logo,
 )

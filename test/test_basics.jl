325c5542d1dda2074f4ad591bb803893dbc3b572
mtfishman <mfishman@flatironinstitute.org>
2025-05-21 10:42:43 -0400

    Define get and getindex

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -1,24 +1,34 @@
 using BackendSelection: BackendSelection, @Algorithm_str, @Backend_str, Algorithm, Backend
 using Test: @test, @testset
+using TestExtras: @constinferred
 
 @testset "BackendSelection" begin
   for type in (Algorithm, Backend)
     @testset "$type" begin
+      # Note: constructing from a string is not type stable.
       @test type("backend") isa type{:backend}
-      @test type(:backend) isa type{:backend}
-      backend = type("backend"; x=2, y=3)
+      @test @constinferred(type(:backend)) isa type{:backend}
+      backend = @constinferred type(:backend; x=2, y=3)
+      @test backend === type("backend"; x=2, y=3)
       @test backend isa type{:backend}
       @test backend.x == 2
       @test backend.y == 3
-      @test propertynames(backend) == (:x, :y)
+      @test @constinferred(getproperty(backend, :x)) == 2
+      @test @constinferred(getproperty(backend, :y)) == 3
+      @test @constinferred(backend[:x]) == 2
+      @test @constinferred(backend[:y]) == 3
+      @test @constinferred(get(backend, :x, "default")) == 2
+      @test @constinferred(get(backend, :y, "default")) == 3
+      @test @constinferred(get(backend, :z, "default")) == "default"
+      @test @constinferred(propertynames(backend)) == (:x, :y)
       (; x, y) = backend
       @test x == 2
       @test y == 3
-      @test BackendSelection.parameters(backend) === (; x=2, y=3)
+      @test @constinferred(BackendSelection.parameters(backend)) === (; x=2, y=3)
     end
   end
   # Macro syntax.
-  @test Algorithm"backend"(; x=2, y=3) === Algorithm("backend"; x=2, y=3)
-  @test Backend"backend"(; x=2, y=3) === Backend("backend"; x=2, y=3)
+  @test @constinferred(Algorithm"backend"(; x=2, y=3)) === Algorithm("backend"; x=2, y=3)
+  @test @constinferred(Backend"backend"(; x=2, y=3)) === Backend("backend"; x=2, y=3)
   @test isnothing(show(Algorithm("")))
 end
